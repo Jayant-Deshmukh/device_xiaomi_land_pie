537966fcec675897f78b747379479292db3656b9
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2018-12-07 10:56:39 +0000

    PixelExperience bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_land.mk
+    $(LOCAL_DIR)/aosp_land.mk
--- aosp_land.mk
+++ aosp_land.mk
@@ -22,7 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Superior stuff
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from land device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -30,7 +33,7 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := superior_land
+PRODUCT_NAME := aosp_land
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
